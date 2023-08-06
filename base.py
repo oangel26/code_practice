01da1b5e338b05971efec362ec5daa977fa0ad87
oangel26 <oa.angel26@gmail.com>
2023-08-06 10:23:08 +0100

    [UPDATE]

--- base.py
+++ base.py
@@ -2,81 +2,81 @@
 import random
  
  
-#games are all the titles of games that the program can use; participants can give their own propositions
+#juegos son todos los títulos de juegos que el programa puede usar; los participantes pueden dar sus propias propuestas
 games = ["Pokemon(1)", "Fortnite(2)", "Minecraft(3)", "CS(4)"]
-#list_of_games_to_guess is the variable into which we insert the password to guess
+#lista_de_juegos_a_adivinar es la variable donde insertamos la contraseña a adivinar
 list_of_games_to_guess = []
-#list_of_entered_games is a list of games entered by the user
+#lista_de_juegos_ingresados es una lista de juegos ingresados por el usuario
 list_of_entered_games = []
-###EVERYTHING ABOVE IS A STARTER PROJECT
+###TODO LO DE ARRIBA ES UN PROYECTO INICIAL
  
  
-#------- QUE ES UNA VARIABLE ---------
+#------- QUÉ ES UNA VARIABLE ---------
 
-#round counter
+#contador de rondas
 number_of_attempts = 0 
  
 
-#-----------QUE ES UN BUCLE - FOR LOOP--------
+#-----------QUÉ ES UN BUCLE - FOR LOOP--------
 
-#we draw passwords to guess
+#elegimos contraseñas para adivinar
 
 for i in range(4):
  game_drawn = random.choice(games)
 
- #append means adding to the list
+ #append significa agregar a la lista
 
  list_of_games_to_guess.append(game_drawn)
  
-#it is a good idea to display the generated password for testing purposes at this stage
+#es una buena idea mostrar la contraseña generada para propósitos de prueba en esta etapa
 #print(list_of_games_to_guess)
-#this line has to be commented out so the player cannot see which games have been drawn. Displaying the list is only needed for testing
+#esta línea debe ser comentada para que el jugador no pueda ver qué juegos han sido elegidos. Mostrar la lista solo es necesario para pruebas
  
-#--------- TEST---------------
+#--------- PRUEBA---------------
 
 
-###STARTER
-#we display the instructions 
-print("Welcome to the MasterMind game")
-print("Your goal is to guess the 4 games that have been drawn by the computer as quickly as possible.")
-print("The order of the games is relevant when guessing.")
-print("Please note the games may be duplicated!!!")
-print("Here is the set of games used by the computer")
+###INICIAL
+#mostramos las instrucciones 
+print("Bienvenido al juego MasterMind")
+print("Tu objetivo es adivinar los 4 juegos que el ordenador ha elegido lo más rápido posible.")
+print("El orden de los juegos es relevante al adivinar.")
+print("¡Ten en cuenta que los juegos pueden ser duplicados!")
+print("Este es el conjunto de juegos utilizado por el ordenador")
 print(games)
 
 
 
 
-#the actual gameplay loop, which will not end until the correct set of games is entered
+#el bucle de juego actual, que no terminará hasta que se introduzca el conjunto correcto de juegos
 while list_of_entered_games != list_of_games_to_guess: 
- #every time we enter games again, we have to clear the list of entered games 
+ #cada vez que ingresamos juegos nuevamente, tenemos que limpiar la lista de juegos ingresados
  list_of_entered_games = []
- #we mark a new attempt
- print("New attempt")
+ #marcamos un nuevo intento
+ print("Nuevo intento")
  print(games)
 
- #we create a loop that executes 4 times
+ #creamos un bucle que se ejecuta 4 veces
  for i in range(4):
-     #with input we enter the title of the next game
-     game_entered = int(input(f"Enter a game no. {i+1}: "))
-     #we search for the game the player has entered in the list of games
+     #con input ingresamos el título del próximo juego
+     game_entered = int(input(f"Ingresa el juego no. {i+1}: "))
+     #buscamos el juego que el jugador ha introducido en la lista de juegos
      game = games[game_entered-1]
-     #append adds the game to the end of the list
+     #append agrega el juego al final de la lista
      list_of_entered_games.append(game)
  
-#------- TEST---------------------------
+#------- PRUEBA---------------------------
 
-# ----------- QUE ES UNA CONDICION------------
+# ----------- QUÉ ES UNA CONDICIÓN------------
 
- #four games so 4 loop runs
+ #cuatro juegos por lo que 4 bucles corren
  for i in range(4):
-     # checking that the games in the same positions match
+     #verificando que los juegos en las mismas posiciones coincidan
      if list_of_entered_games[i] == list_of_games_to_guess[i]:
-        print(f"{i+1} - {list_of_entered_games[i]} - Correct")
+        print(f"{i+1} - {list_of_entered_games[i]} - Correcto")
      else:
-        print(f"{i+1} - {list_of_entered_games[i]} - Incorrect")
+        print(f"{i+1} - {list_of_entered_games[i]} - Incorrecto")
  number_of_attempts+=1
- print(f"number of attempts: {number_of_attempts}")
+ print(f"número de intentos: {number_of_attempts}")
 
 
-#------------ TEST------------------
+#------------ PRUEBA------------------
